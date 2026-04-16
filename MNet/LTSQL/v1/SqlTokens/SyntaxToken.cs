5cd8e44bb7f874be478aa4c154c8d61d500bdc30
yaomz <yaomz@yizit.cn>
2026-04-16 13:51:28 +0800

    SyntaxToken 文件精简

--- MNet/LTSQL/v1/SqlTokens/SyntaxToken.cs
+++ MNet/LTSQL/v1/SqlTokens/SyntaxToken.cs
@@ -7,18 +7,11 @@ namespace MNet.LTSQL.v1.SqlTokens
     //直接表示SQL语法编码的一部分
     public class SyntaxToken : LTSQLToken
     {
-        public SyntaxToken()
-        { }
         public SyntaxToken(string text)
         {
             this.Text = text;
         }
-        public SyntaxToken(string text, bool escape)
-        {
-            this.Text = text;
-            this.EscapeKey = escape;
-        }
-
+        
 
         public readonly string Text;
         //是否需要关键字转义
@@ -33,17 +26,10 @@ namespace MNet.LTSQL.v1.SqlTokens
         {
             return new SyntaxToken(text);
         }
-        public static SyntaxToken Create(string text, bool escape)
-        {
-            return new SyntaxToken(text, escape);
-        }
         public static SyntaxToken[] CreateBatch(params string[] texts)
         {
             SyntaxToken[] tokens = texts.Select(txt => Create(txt)).ToArray();
             return tokens;
         }
     }
-
-    public class SqlTxt 
-    { }
 }
